f63874cb4a1486701730736f3f35355027cf79d9
Lucas Berryman <s4021116@student.rmit.edu.au>
2026-05-31 21:02:43 +1000

    Add compressed podcast audio

---
+++ finalize_index_podcast_indent.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+p = Path(r'e:\Uni stuff 2 bachelors\Digital fluency\Ass 4\Repo\Digital-Fluency-Assessment-4\index.html')
+text = p.read_text('utf-8')
+old = "\t\t\t\t\t<ul class=\"actions\">\n\t\t\t\t\t\t\t<li><a href=\"full-podcast.html\" class=\"button primary\">Listen to full episode</a></li>\n\t\t\t\t\t</ul>\n\t\t\t\t\t<p class=\"note\">"
+new = "\t\t\t\t\t\t<ul class=\"actions\">\n\t\t\t\t\t\t\t<li><a href=\"full-podcast.html\" class=\"button primary\">Listen to full episode</a></li>\n\t\t\t\t\t\t</ul>\n\t\t\t\t\t\t<p class=\"note\">"
+if old in text:
+    text = text.replace(old, new, 1)
+    p.write_text(text, 'utf-8')
+    print('updated')
+else:
+    print('not found')
